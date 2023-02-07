36687a61ca9a66874f7d7a5fa418ae19bc5d5a9e
defylol <66927638+defylol@users.noreply.github.com>
2023-02-06 22:01:16 -0700

    Update server.lua

--- dTackle/server.lua
+++ dTackle/server.lua
@@ -1,7 +1,3 @@
-SetConvarReplicated("TackleKey", config.TackleKey)
-SetConvarReplicated("TackleTime", config.TackleTime)
-SetConvarReplicated("TackleAntiCheatDistanceLimit", config.TackleAntiCheatDistanceLimit)
-
 RegisterServerEvent('Tackle:Server:TacklePlayer')
 AddEventHandler('Tackle:Server:TacklePlayer', function(src, targ)
 	TriggerClientEvent("Tackle:Client:TacklePlayer", targ, src)
